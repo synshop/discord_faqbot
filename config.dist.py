df54071b6e842e9639e1bd1b7fc856d59313bf38
Brian Munroe <brian.e.munroe@gmail.com>
2022-10-14 11:34:38 -0700

    updating a few things

--- config.dist.py
+++ config.dist.py
@@ -1,1 +1,42 @@
 TOKEN = "<DISCORD OAUTH2 TOKEN GOES HERE"
+
+PHRASES = [
+    "IS THE SHOP OPEN",
+    "SHOP HOURS",
+    "IS ANYONE AT THE SHOP NOW",
+    "IS ANY ONE AT THE SHOP NOW",
+    "IS ANYONE AT THE SHOP",
+    "IS ANY ONE AT THE SHOP",
+    "IS ANYONE GOING TO BE AT THE SHOP TODAY",
+    "IS ANY ONE GOING TO BE AT THE SHOP TODAY",
+    "IS ANYONE GOING TO BE AT THE SHOP TOMORROW",
+    "IS ANY ONE GOING TO BE AT THE SHOP TOMORROW",
+    "IS ANYONE GOING TO BE AT THE SHOP TONIGHT",
+    "IS ANY ONE GOING TO BE AT THE SHOP TONIGHT",
+    "ANY PLANS FOR THE SHOP TO BE OPEN",
+    "ANYONE GOING TONIGHT",
+    "ANY ONE GOING TONIGHT",
+    "IS ANYONE GOING TO THE SHOP TONIGHT",
+    "IS ANY ONE GOING TO THE SHOP TONIGHT"
+]
+
+SHOP_HOURS = """
+```
+Current Shop Hours as of Oct 2022
+===
+Monday:     7-10pm (1st and 3rd Mondays each month only)
+Tuesday:    7-10pm
+Wednesday:  7-10pm
+Thursday:   Closeed
+Friday:     6-10pm
+Saturday:   Noon to 5pm
+Sunday:     Closed
+
+5967 Harrison Dr
+Las Vegas, NV, 89120
+
+The times listed are hours that vetted members have volunteered to hold the shop open for non-vetted members.  We are still closed to the public pending the finalization of our business permit, although people can contact us to schedule tours ahead of time as needed.
+
+This listing will be updated as more vetted members step up to hold the shop open. If you are vetted and willing to volunteer, please let one of the board members know.
+```
+"""
