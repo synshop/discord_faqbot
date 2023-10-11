d0d00c092c88717d1a4898d0775363f703ea3ae6
Brian Munroe <brian@synshop.org>
2023-10-11 14:11:10 -0700

    working on ISSUE-4

--- main.py
+++ main.py
@@ -24,13 +24,13 @@ def get_shop_hours():
         td = tr.find_all("td")
         hours_dict[td[0].text] = td[1].text
 
-    markdown_string = "```\nCurrent Shop Hours\n=====\n"
+    markdown_string = """```\nCurrent Shop Hours\n=====\n"""
 
     for k,v in hours_dict.items():
-        markdown_string = markdown_string + k + ":\U00000009\U00000009" + v + "\n"
+        markdown_string = markdown_string + k + """:\t\t""" + v + """\n""""
 
-    markdown_string = markdown_string + "\n5967 Harrison Dr\nLas Vegas, NV, 89120\n"
-    markdown_string = markdown_string + "```"
+    markdown_string = markdown_string + """\n5967 Harrison Dr\nLas Vegas, NV, 89120\n"""
+    markdown_string = markdown_string + """```"""
 
     return(markdown_string)
 
