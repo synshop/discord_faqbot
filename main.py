62d14275351b917bdf53d8850dad22b6968084c7
Brian Munroe <brian@synshop.org>
2023-10-11 14:02:48 -0700

    working on ISSUE-4

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ def get_shop_hours():
     markdown_string = "```\nCurrent Shop Hours\n=====\n"
 
     for k,v in hours_dict.items():
-        markdown_string = markdown_string + k + ":\t\t" + v + "\n"
+        markdown_string = markdown_string + k + ":\t" + v + "\n"
 
     markdown_string = markdown_string + "\n5967 Harrison Dr\nLas Vegas, NV, 89120\n"
     markdown_string = markdown_string + "```"
