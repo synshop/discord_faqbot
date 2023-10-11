58b8065a245e3b5a419299bf61f7a21819974e16
Brian Munroe <brian@synshop.org>
2023-10-11 13:55:22 -0700

    working on ISSUE-3

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import discord
 from discord.ext import commands
+import requests
+from bs4 import BeautifulSoup
+
 import random, string
 
 import config, discord_token
@@ -9,6 +12,27 @@ intents.message_content = True
 intents.messages = True
 
 client = discord.Client(intents=intents)
+
+def get_shop_hours():
+    r = requests.get(config.SHOP_HOURS_URL)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    shop_hours = soup.find(id="shophours")
+
+    hours_dict = {}
+
+    for tr in shop_hours.find_all('tr'):
+        td = tr.find_all("td")
+        hours_dict[td[0].text] = td[1].text
+
+    markdown_string = "```\n"
+
+    for k,v in hours_dict.items():
+        markdown_string = markdown_string + k + ":\t\t" + v + "\n"
+
+    markdown_string = markdown_string + "```"
+
+    return(markdown_string)
+
 
 @client.event
 async def on_ready():
@@ -25,6 +49,6 @@ async def on_message(message):
 
     for phrase in config.PHRASES:
         if m == phrase:
-            await message.channel.send(config.SHOP_HOURS)
+            await message.channel.send(get_shop_hours())
 
 client.run(discord_token.TOKEN)
