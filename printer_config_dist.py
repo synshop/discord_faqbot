19b6eb8f38e1bf79d528a4f6159ea12421496501
Brian Munroe <brian@synshop.org>
2024-12-17 14:43:40 -0800

    adding the ability to query 3d printers (issue-13)

---
+++ printer_config_dist.py
@@ -0,0 +1,22 @@
+PRINTERS = {
+    "BDP-OOM-999": {
+        "name": "Printer 1",
+        "ip": "127.0.0.1",
+        "port": 8883,
+        "mac_address": "C0:F5:35:23:3A:11",
+        "serial": "00M09A3C1400999",
+        "username":"bblp",
+        "access_code": "xxxxxxx",
+        "topic_name": "device/00M09A3C1400999/report"
+    },
+    "3DP-00M-998": {
+        "name": "Printer 2",
+        "ip": "127.0.0.1",
+        "port": 8883,
+        "mac_address": "50:41:1C:68:1B:11",
+        "serial": "00m09c422700998",
+        "username":"bblp",
+        "access_code": "xxxxxxxx",
+        "topic_name": "device/00m09c422700998/report"
+    }
+}
