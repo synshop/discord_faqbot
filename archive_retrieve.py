2212cbcad614d2c44af662c7f35930621ecaaf6a
mrjones-plip <mrjones-plip@plip.com>
2025-01-04 09:30:42 -0800

    WIP - got screenshot and DB working nicely

--- archive_retrieve.py
+++ archive_retrieve.py
@@ -1,5 +1,6 @@
 import paho.mqtt.subscribe as subscribe
-import json, ssl, config, requests, sqlite3, datetime
+import json, ssl, config, requests, sqlite3, pytz, av, time, PIL
+from datetime import datetime, timezone
 from bs4 import BeautifulSoup
 from printer_config import PRINTERS
 
@@ -10,41 +11,70 @@ def get_database_handle():
 
     create_table_statement = """CREATE TABLE IF NOT EXISTS print_status (
         id INTEGER PRIMARY KEY,
-        date TIMESTAMP,  
+        date DATETIME NOT NULL,  
         printer text NOT NULL, 
         printer_id text NOT NULL, 
         state text NOT NULL, 
-        raw_object text NOT NULL,
         job text, 
-        mins text
+        mins text,
+        task_id text,
+        image BLOB
     );"""
+
     try:
-        with sqlite3.connect(db_file) as database:
+        with sqlite3.connect(
+                db_file,
+                detect_types=sqlite3.PARSE_DECLTYPES |
+                sqlite3.PARSE_COLNAMES) as database:
             cursor = database.cursor()
             cursor.execute(create_table_statement)
             database.commit()
     except sqlite3.OperationalError as e:
         print("Failed to open database", db_file, ". Error is:", e)
+        exit(1)
 
     return database
 
+# Thanks https://github.com/Cacsjep/pyrtsputils/blob/main/snapshot_generator.py
+def save_image(ip, password):
+    url = "rtsps://bblp:" + password + "@" + ip + ":322/streaming/live/1"
+    video = av.open(url, 'r')
+    # Iter over Package to get an frame
+    for packet in video.demux():
+        # When frame is decoded
+        for frame in packet.decode():
+            # Current datetime
+            ts = time.strftime("%Y%m%d-%H%M%S")
+            # Log file loctaion of file
+            print(f'Save Frame {ip}.jpg')
+            # Save Frame into JPEG
+            frame.to_image().save(f'{ip}.jpg')
+            # Close Connection to RTSP Source
+            video.close()
+            print('Closing Connection')
+            # Return because we just need one frame
+            return
 
-def save_printer_status(printer, printer_id, state, raw_object, job, mins, database):
-    currentDateTime = datetime.datetime.now()
-    sql = '''INSERT INTO print_status(date, printer, printer_id, state, raw_object, job, mins)
-             VALUES(?,?,?,?,?,?,?) '''
 
+def save_printer_status(status_obj, database):
+    time_zone = pytz.timezone('US/Pacific')
+    current_date_time = datetime.now(time_zone)
+    sql = '''INSERT INTO 
+                print_status(date, printer, printer_id, state, job, mins, task_id)
+             VALUES
+                (?,?,?,?,?,?,?)'''
+    print(status_obj)
     row = (
-        currentDateTime,
-        printer,
-        printer_id,
-        state,
-        str(raw_object),
-        job,
-        mins
+        current_date_time,
+        status_obj[0],
+        status_obj[1],
+        status_obj[2],
+        status_obj[3],
+        status_obj[4],
+        status_obj[5]
     )
     cursor = database.cursor()
-    # print(row)
+    print(row)
     cursor.execute(sql, row)
 
     # commit the changes
@@ -54,30 +84,36 @@ def save_printer_status(printer, printer_id, state, raw_object, job, mins, datab
     return cursor.lastrowid
 
 
-def get_printer_status():
-    status = ""
+def get_printer_status(printer, printer_id):
+    auth = {"username": printer["username"], "password": printer["access_code"]}
+    tls = ssl._create_unverified_context()
+
+    try:
+        msg = subscribe.simple(topics=printer["topic_name"], hostname=printer["ip"], port=printer["port"], auth=auth, tls=tls)
+        printer_object = json.loads(msg.payload)
+
+        name = printer["name"]
+        printer_id = printer_id
+        state = printer_object["print"]["gcode_state"]
+        job = printer_object["print"]["subtask_name"]
+        mins = printer_object["print"]["mc_remaining_time"]
+        task_id = printer_object["print"]["task_id"]
+        return name, printer_id, state, job, mins, task_id
+    except Exception as e:
+        print(f'failed to get printer status for {printer["name"]} ({printer["ip"]}:{printer["port"]})', e)
+
+
+def loop_over_printers():
     database = get_database_handle()
 
     for printer_id in PRINTERS:
         printer = PRINTERS[printer_id]
-
-        auth = {"username": printer["username"], "password": printer["access_code"]}
-        tls = ssl._create_unverified_context()
-
-        msg = subscribe.simple(topics=printer["topic_name"], hostname=printer["ip"], port=printer["port"], auth=auth,
-                               tls=tls)
-        printer_object = json.loads(msg.payload)
-
-        name = printer["name"]
-        job = printer_object["print"]["subtask_name"]
-        state = printer_object["print"]["gcode_state"]
-        mins = printer_object["print"]["mc_remaining_time"]
-
-        save_printer_status(name, printer_id, state, printer_object, job, mins, database)
-
-        # print(name, state, mins, job, printer_id)
+        status_obj = get_printer_status(printer, printer_id)
+        save_printer_status(status_obj, database)
+        save_image(printer["ip"], printer["access_code"])
 
     database.close()
+
 
 def get_shop_hours():
     r = requests.get(config.SHOP_HOURS_URL)
@@ -106,5 +142,5 @@ def get_shop_hours():
     return markdown_string
 
 
-get_printer_status()
+loop_over_printers()
 # print(get_shop_hours())
