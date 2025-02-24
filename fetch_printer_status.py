ce87d6215634d2df604ef797dd9621f9e283232b
Brian Munroe <brian@synshop.org>
2025-02-24 12:24:12 -0800

    Update fetch_printer_status.py
    Looks good, works for me
    
    Co-authored-by: mrjones <8253488+mrjones-plip@users.noreply.github.com>

--- fetch_printer_status.py
+++ fetch_printer_status.py
@@ -1,16 +1,17 @@
-import json
-from data.printer_config import PRINTERS
 import fdmprinting.archive_retrieve as ar
 
+
 def get_printer_status():
+    for id in ar.PRINTERS:
+        printer = ar.PRINTERS[id]
+        file = printer["name"] + ".json"
+        print("Writing " + file)
+        printer_object = ar.get_status_from_mqtt(printer, id)
+        printer_object["raw_json"] = ar.clean_raw_json(printer_object["raw_json"])
+        with open(file, "w") as my_file:
+            my_file.write(ar.json.dumps(printer_object, indent=4))
+    print("Done")
 
-    for p in PRINTERS:
-        printer = PRINTERS[p]
-        printer_object = ar.get_status_from_mqtt(printer, printer["name"])
-        clean_json = ar.clean_raw_json(printer_object["raw_json"])
-
-        with open("./"+ printer["name"] + ".json", "w") as my_file:
-            my_file.write(json.dumps(clean_json, indent=4))
 
 if __name__ == "__main__":
     get_printer_status()
